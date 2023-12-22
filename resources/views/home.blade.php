3964d7d261d5c6e4a1f44cc29397490eb792ab26
teoma145 <matteodark145@yahoo.it>
2023-12-22 16:33:46 +0100

    text-in-main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,9 @@
 @section('content')
 <main>
     <h1>Home page</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem debitis adipisci eveniet laudantium ratione nihil perspiciatis voluptatem,
+        suscipit unde iusto molestiae eum hic,
+        vitae et, ex ea aliquid explicabo consectetur? Ipsa qui sapiente animi illum in tempora aperiam. Minima, hic?</p>
 </main>
 
 @endsection
